db43a0c379a388b124ac9d54839f331e2e0cb930
Shamrylo Mykola <itlutsk@gmail.com>
2022-08-18 11:25:23 +0300

    Fix active category menu item 2022-08-18

--- widgets/MenuWidget.php
+++ widgets/MenuWidget.php
@@ -15,6 +15,36 @@ class MenuWidget extends Widget
 
         $this->data = $model_category->getCategories();
 
+        $current = \Yii::$app->request->getPathInfo();
+
+        if($current === '')
+        {
+            array_unshift($this->data, [
+                'id' => 0,
+                'cat_name' => '',
+                'browser_name' => 'Всё меню',
+                'active' => 1
+            ]);
+        } else
+        {
+            array_unshift($this->data, [
+                'id' => 0,
+                'cat_name' => '',
+                'browser_name' => 'Всё меню',
+                'active' => 0
+            ]);
+        }
+
+        foreach($this->data as $key => $value)
+        {
+            if($value['cat_name'] == str_replace('category/', '', $current))
+            {
+                $this->data[$key]['active'] = 1;
+            } else {
+                $this->data[$key]['active'] = 0;
+            }
+        }
+
         return $this->categoryRender($this->data);
     }
 
